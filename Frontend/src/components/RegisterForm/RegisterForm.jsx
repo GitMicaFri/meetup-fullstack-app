07ca52a2d26130be0a413e4842d05ca6345ef360
J0113boy <keskitalo_6@hotmail.com>
2024-11-01 10:21:58 +0100

    test

--- Frontend/src/components/RegisterForm/RegisterForm.jsx
+++ Frontend/src/components/RegisterForm/RegisterForm.jsx
@@ -1,26 +1,28 @@
-
 const RegisterForm = () => {
-    return(
-        <div className="container">
+  return (
+    <div className="container">
+      <div>
+        <h1 className="title">Fullstack Meetups!</h1>
+      </div>
 
-            <div>
-                <h1 className="title">Fullstack Meetups</h1>
-            </div>
+      <section className="section">
+        <h2 className="subtitle">Registrera dig här</h2>
 
-            <section className="section">
-                <h2 className="subtitle">Registrera dig här</h2>
+        <div className="form">
+          <input type="text" placeholder="Förnamn" className="input" />
+          <input type="text" placeholder="Efternamn" className="input email" />
+          <input type="text" placeholder="Email" className="input email" />
+          <input
+            type="password"
+            placeholder="Lösenord"
+            className="input password"
+          />
 
-                <div className="form">
-                    <input type="text" placeholder="Förnamn" className="input" />
-                    <input type="text" placeholder="Efternamn" className="input email" />
-                    <input type="text" placeholder="Email" className="input email" />
-                    <input type="password" placeholder="Lösenord" className="input password" />
-
-                    <button className="button">Registrera</button>
-                </div>
-            </section>
-       </div>
-    )
+          <button className="button">Registrera</button>
+        </div>
+      </section>
+    </div>
+  )
 }
 
-export default RegisterForm;
+export default RegisterForm
