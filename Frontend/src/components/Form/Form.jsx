ef46f06c2318c5b44fcf04dd89d78d9c3a56009b
J0113boy <keskitalo_6@hotmail.com>
2024-11-01 10:56:25 +0100

    testing s3 bucket

--- Frontend/src/components/Form/Form.jsx
+++ Frontend/src/components/Form/Form.jsx
@@ -1,34 +1,39 @@
-import "./Form.css";
-import { useNavigate } from "react-router-dom";
+import "./Form.css"
+import { useNavigate } from "react-router-dom"
 
 const Form = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate()
 
-    function handleClick() {
-        navigate('/register')
-    }
+  function handleClick() {
+    navigate("/register")
+  }
 
-    return(
-       <div className="container">
+  return (
+    <div className="container">
+      <div>
+        <h1 className="title">Fullstack Meetups!!!</h1>
+      </div>
 
-            <div>
-                <h1 className="title">Fullstack Meetups</h1>
-            </div>
+      <section className="section">
+        <h2 className="subtitle">Logga in</h2>
 
-            <section className="section">
-                <h2 className="subtitle">Logga in</h2>
+        <div className="form">
+          <input type="text" placeholder="Email" className="input email" />
+          <input
+            type="password"
+            placeholder="Lösenord"
+            className="input password"
+          />
 
-                <div className="form">
-                    <input type="text" placeholder="Email" className="input email" />
-                    <input type="password" placeholder="Lösenord" className="input password" />
+          <p className="register-btn" onClick={handleClick}>
+            Inget konto? Registrera dig här!
+          </p>
 
-                    <p className="register-btn" onClick={handleClick}>Inget konto? Registrera dig här!</p>
-
-                    <button className="button">Logga in</button>
-                </div>
-            </section>
-       </div>
-    )
+          <button className="button">Logga in</button>
+        </div>
+      </section>
+    </div>
+  )
 }
 
-export default Form;
+export default Form
