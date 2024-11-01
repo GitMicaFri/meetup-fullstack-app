7f928891d359ab5100a7f078dbb6da4cb228395d
Topler97 <yhjs23.toplam@folkuniversitetet.nu>
2024-11-01 14:55:42 +0100

    Created meetup
    created meetup

---
+++ Frontend/src/Pages/Landing/Landing.jsx
@@ -0,0 +1,21 @@
+import Form from "../../components/Form/Form";
+
+
+const Landing = () => {
+
+    return(
+
+        <div className="container">
+
+             <div>
+                <h1 className="title">Fullstack Meetups</h1>
+             </div>
+
+             <Form />
+
+
+        </div>
+    )   
+}
+
+export default Landing;
