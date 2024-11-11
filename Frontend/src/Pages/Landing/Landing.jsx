2e6e92e3d02c38e8e4cb7db172e04749281e18de
J0113boy <keskitalo_6@hotmail.com>
2024-11-11 13:34:34 +0100

    Add register here link on landing-page

--- Frontend/src/Pages/Landing/Landing.jsx
+++ Frontend/src/Pages/Landing/Landing.jsx
@@ -1,15 +1,17 @@
-import Form from "../../components/Form/Form";
-
+import { Link } from "react-router-dom"
+import Form from "../../components/Form/Form"
 
 const Landing = () => {
+  return (
+    <div className="container">
+      <h1 className="title">Fullstack Meetups</h1>
 
-    return(
-        <div className="container">
-            <h1 className="title">Fullstack Meetups</h1>
-
-            <Form />
-        </div>
-    )   
+      <Form />
+      <p className="register-text">
+        Har du ingen användare? <Link to="/register">Registrera dig här</Link>
+      </p>
+    </div>
+  )
 }
 
-export default Landing;
+export default Landing
